a76e4983ba1a0dd6be9c50229cd70e522b3b96be
IamHatim09 <blackfeather0909@gmail.com>
2026-09-12 10:39:08 +0530

    changed dendritic.nix

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -1,7 +1,4 @@
-{inputs, den, ...}: {
-
-  _module.args.__findfile = den.lib.__findfile;
-  
+{ inputs, den, ... }: {
   imports = [
     (inputs.flake-file.flakeModules.dendritic or {})
     (inputs.den.flakeModules.dendritic or {})
